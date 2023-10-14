74b0d926a735dbc59662912f8962f25fa6bbcb6d
DM <druavhm@gmail.com>
2023-10-14 23:35:08 +0530

    Basic Logic Added

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("hey")
+console.log("JS Working");
+let firstCard = prompt("No between 2-11 : ");
+let secondCard = prompt("No between 2-11 : ");
+firstCard = Number.parseInt(firstCard);
+secondCard = Number.parseInt(secondCard);
+let sum = firstCard + secondCard;
+console.log("Sum is : ", sum);
+
+if (sum <= 20) {
+  console.log("Do you want to draw a new card?");
+} else if (sum === 21) {
+  //=== means "strictly equal to"
+  console.log("WOHOOOOO! You got a Blackjack!");
+} else {
+  console.log("I aint playingggg anymore!!!!");
+}
