aff9d72461f2211044c81f3755c07d79057b02a2
DM <druavhm@gmail.com>
2023-10-15 00:28:33 +0530

    basic style added

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 console.log("JS Working");
-let firstCard = prompt("No between 2-11 : ");
-let secondCard = prompt("No between 2-11 : ");
+let firstCard = 1//prompt("No between 2-11 : ");
+let secondCard =2 //prompt("No between 2-11 : ");
 firstCard = Number.parseInt(firstCard);
 secondCard = Number.parseInt(secondCard);
 let sum = firstCard + secondCard;
 let hasBlackjack = false;
 let isAlive = true;
+let message = ""
 
 
 console.log("Sum is : ", sum);
 
 if (sum <= 20) {
-  console.log("Do you want to draw a new card?");
+  message = ("Do you want to draw a new card?");
 } else if (sum === 21) {
   //=== means "strictly equal to"
-  console.log("WOHOOOOO! You got a Blackjack!");
+  message = ("WOHOOOOO! You got a Blackjack!");
   hasBlackjack = true;
 } else {
-  console.log("I aint playingggg anymore!!!!");
+  message = ("I aint playingggg anymore!!!!");
   isAlive = false;
 }
 console.log(isAlive, hasBlackjack)
+console.log(message)
