89fd7c201ce0a0290e7fd20508d15b79adc23233
DM <druavhm@gmail.com>
2023-10-17 00:09:12 +0530

    start game function added and now working

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
 console.log("JS Working");
-let firstCard = 1//prompt("No between 2-11 : ");
-let secondCard =2 //prompt("No between 2-11 : ");
+let firstCard = prompt("No between 2-11 : ");
+let secondCard = prompt("No between 2-11 : ");
 firstCard = Number.parseInt(firstCard);
 secondCard = Number.parseInt(secondCard);
 let sum = firstCard + secondCard;
 let hasBlackjack = false;
 let isAlive = true;
-let message = ""
-
-
+let message = "";
+let message_el = document.getElementById("messageEl");
+let sum_el = document.getElementById("sumEl");
+let cards_el = document.getElementById("cardsEl");
 
 console.log("Sum is : ", sum);
 
 function startGame() {
-
-  
   if (sum <= 20) {
-  message = ("Do you want to draw a new card?");
-} else if (sum === 21) {
-  //=== means "strictly equal to"
-  message = ("WOHOOOOO! You got a Blackjack!");
-  hasBlackjack = true;
-} else {
-  message = ("I aint playingggg anymore!!!!");
-  isAlive = false;
-}
-console.log(isAlive, hasBlackjack)
-console.log(message)
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    //=== means "strictly equal to"
+    message = "WOHOOOOO! You got a Blackjack!";
+    hasBlackjack = true;
+  } else {
+    message = "I aint playingggg anymore!!!!";
+    isAlive = false;
+  }
+  cards_el.textContent += " " + firstCard + ", " + secondCard;
+  sum_el.textContent += " " + sum;
+  message_el.textContent = message;
+  // console.log(isAlive, hasBlackjack)
+  console.log(message);
 }
