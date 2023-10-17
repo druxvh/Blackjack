c17d10ec0fff28309011b44dd4cf02969c9bdd6d
DM <druavhm@gmail.com>
2023-10-17 23:39:20 +0530

    added a new card button

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log("JS Working");
-let firstCard = prompt("No between 2-11 : ");
-let secondCard = prompt("No between 2-11 : ");
+let firstCard = 5 //prompt("No between 2-11 : ");
+let secondCard = 5 //prompt("No between 2-11 : ");
 firstCard = Number.parseInt(firstCard);
 secondCard = Number.parseInt(secondCard);
 let sum = firstCard + secondCard;
@@ -11,22 +11,38 @@ let message_el = document.getElementById("messageEl");
 let sum_el = document.getElementById("sumEl");
 let cards_el = document.getElementById("cardsEl");
 
-console.log("Sum is : ", sum);
+
 
 function startGame() {
+  renderGame()
+}
+
+
+function renderGame() {
+  cards_el.textContent = "Cards : " + firstCard + ", " + secondCard;
+  sum_el.textContent = "Sum : " + sum;
+  
   if (sum <= 20) {
     message = "Do you want to draw a new card?";
   } else if (sum === 21) {
     //=== means "strictly equal to"
-    message = "WOHOOOOO! You got a Blackjack!";
+    message = "You got a Blackjack!";
     hasBlackjack = true;
   } else {
-    message = "I aint playingggg anymore!!!!";
+    message = "You loser!";
     isAlive = false;
   }
-  cards_el.textContent += " " + firstCard + ", " + secondCard;
-  sum_el.textContent += " " + sum;
   message_el.textContent = message;
-  // console.log(isAlive, hasBlackjack)
-  console.log(message);
+}
+
+function newCard() {
+  console.log("Nw card")
+let new_card = 4;
+sum += new_card
+
+
+cards_el.textContent += ", " + new_card;
+
+renderGame()
+
 }
